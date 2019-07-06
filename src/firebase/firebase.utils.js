e34f8257e17b3be7076d2cb5cb18a39ec9b2a977
Ron Ganguly <c1ph3r9@gmail.com>
2019-07-05 22:50:50 -0500

    implemented firebase utils, store authenticated user to db

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,6 +11,27 @@ const config = {
 	messagingSenderId: '952105743425',
 	appId: '1:952105743425:web:a0aac76f0e5a6485'
 };
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+	if (!userAuth) return;
+	const userRef = firestore.doc(`users/${userAuth.uid}`);
+	const snapShot = await userRef.get();
+	if (!snapShot.exist) {
+		const { displayName, email } = userAuth;
+		const createdAt = new Date();
+		try {
+			await userRef.set({
+				displayName,
+				email,
+				createdAt,
+				...additionalData
+			});
+		} catch (err) {
+			console.log('error creating user', err.message);
+		}
+	}
+	return userRef;
+};
+
 firebase.initializeApp(config);
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
